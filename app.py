ce137d50dbd7ec645beb76760ba7d66e2d88cb6c
Riyas j <98734565+riyasjhussain@users.noreply.github.com>
2023-04-02 19:05:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
-
 import streamlit as st
 import tensorflow.keras as keras
 from PIL import Image
 from tensorflow.keras.utils import load_img,img_to_array
 import numpy as np
 
+
+
 import keras.utils as image
-from tensorflow.keras.applications.resnet import preprocess_input
+from tensorflow.keras.applications.vgg19 import preprocess_input
 model = keras.models.load_model("mritest.h5")
 st.set_page_config(
-   
+    page_title="MRImage Classifier",
     page_icon=":art:",
     layout="wide",
     initial_sidebar_state="expanded"
 )
 
-target_size = (176, 208)
-uploaded_file = st.file_uploader('Choose an image...', type=['jpg', 'jpeg', 'png'])
+uploaded_file = st.file_uploader("Choose image", type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file is not None:
     
-    image = Image.open(uploaded_file)
-    image = image.resize(target_size)
-    image_array = np.array(image)
+    
+    img=image.load_img(uploaded_file,target_size=(176,208))
+
+    i=image.img_to_array(img)
+    i=preprocess_input(i)
+    input_arr=np.array([i])
     input_arr = input_arr/255.0
     
-    image_array = np.expand_dims(image_array, axis=0)
     # input_arr=np.expand_dims(input_arr,axis=0)
-    y_predict=np.argmax(model.predict(image_array))
+    y_predict=np.argmax(model.predict(input_arr))
     y_predict
     if y_predict==0:
         st.write("MildDemented")
@@ -41,8 +41,16 @@ if uploaded_file is not None:
          st.write("ModerateDemented")
     elif y_predict==2:
          st.write("NonDemented")
+    
+    
+
     else:
-         st.write("VeryMildDemented")
+         st.write("verymild")
+
+
+
+
+
 
 
 
